13e6a17e6c1322454dad23a22d3cd03767f96376
Aravindra Prasad <aravindraprasad16@gmail.com>
2025-07-27 22:07:51 +0530

    Some code changes in kernel

--- kernel.py
+++ kernel.py
@@ -1,12 +1,14 @@
 import cv2
 import numpy as np
 
-img = cv2.imread("image.png", cv2.IMREAD_GRAYSCALE) 
-
+img = cv2.imread("img/image.png", cv2.IMREAD_GRAYSCALE) 
+width = 300
+height = 300
+img = cv2.resize(img,(width,height))
 kernel = np.array([[0, -1,  0],
                    [-1, 5, -1],
                    [0, -1,  0]], dtype=np.float32)  
-
+print(type(img))
 h, w = img.shape
 out = np.zeros_like(img, dtype=np.float32)
 
@@ -16,7 +18,7 @@ for y in range(1, h-1):
         out[y, x] = np.sum(patch * kernel)
 
 out = np.clip(out, 0, 255).astype(np.uint8)
-
+print(out)
 cv2.imshow("original", img)
 cv2.imshow("filtered", out)
 cv2.waitKey(0)
